ddcde884124a80d9c17472cd62def6725fe0f7ee
Jakub Duma <jduma842@gmail.com>
2024-06-05 21:18:21 +0200

    Dodano ruch snake'a

--- SnakeApp/Program.cs
+++ SnakeApp/Program.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading;
-
-namespace SnakeGame
+﻿namespace SnakeGame
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Console.CursorVisible = false;
             SnakeGame game = new SnakeGame();
@@ -14,7 +10,7 @@ namespace SnakeGame
         }
     }
 
-    class SnakeGame
+    internal class SnakeGame
     {
         private int _width = 40;
         private int _height = 20;
@@ -35,7 +31,7 @@ namespace SnakeGame
                     //ChangeDirection(key);
                 }
                 DrawBoard();
-                //MoveSnake();
+                MoveSnake();
                 //CheckCollision();
                 Thread.Sleep(100);
             }
@@ -43,6 +39,7 @@ namespace SnakeGame
             Console.WriteLine("Game Over");
             Console.WriteLine($"Your score: {_score}");
         }
+
         private void InitializeGame()
         {
             _snake.Add(new Position(5, 5));
@@ -77,15 +74,53 @@ namespace SnakeGame
             }
             Console.WriteLine($"Score: {_score}");
         }
+
+        private void MoveSnake()
+        {
+            Position head = _snake[0];
+            Position newHead = head;
+
+            switch (_direction)
+            {
+                case Direction.Right:
+                    newHead = new Position(head.X, head.Y + 1);
+                    break;
+
+                case Direction.Left:
+                    newHead = new Position(head.X, head.Y - 1);
+                    break;
+
+                case Direction.Up:
+                    newHead = new Position(head.X - 1, head.Y);
+                    break;
+
+                case Direction.Down:
+                    newHead = new Position(head.X + 1, head.Y);
+                    break;
+            }
+
+            _snake.Insert(0, newHead);
+            if (newHead.Equals(_food))
+            {
+                _score++;
+                GenerateFood();
+            }
+            else
+            {
+                _snake.RemoveAt(_snake.Count - 1);
+            }
+        }
     }
-    enum Direction
+
+    internal enum Direction
     {
         Right,
         Left,
         Up,
         Down
     }
-    struct Position
+
+    internal struct Position
     {
         public int X;
         public int Y;
